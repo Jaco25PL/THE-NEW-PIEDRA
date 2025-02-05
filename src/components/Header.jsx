994e68b441dbfcff46fbcc207f417f5343447874
PiedraL <joaquinlangone12@gmail.com>
2025-02-05 18:22:52 -0300

    working in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,27 +7,36 @@ export function Header() {
             <nav className="header-grid">
               {/* 1) First column: Social icons (subdivided into three) */}
               <div className="column social-column">
-                <img src="./images/instagram-logo.svg" alt="Instagram" />
-                <img src="./images/facebook-logo.svg" alt="Facebook" />
-                <img src="./images/youtube-logo.svg" alt="YouTube" />
+
+
+                <a href="http://" target="_blank" ><img src="./images/instagram-logo.svg" alt="Instagram" /></a>
+                <a href="http://" target="_blank" ><img src="./images/facebook-logo.svg" alt="Facebook" /></a>
+                <a href="http://" target="_blank" ><img src="./images/youtube-logo.svg" alt="YouTube" /></a>
+
+
+                
+                
+                
               </div>
 
               {/* 2) Second column */}
-              <div className="column"><a>HISTORIA</a> </div>
+              <div className="column"><a href='#'>HISTORIA</a> </div>
 
               {/* 3) Third column */}
-              <div className="column"><a>CONSTRUCCIONES</a> </div>
+              <div className="column"><a href='#'>CONSTRUCCIONES</a> </div>
 
               {/* 4) Fourth column (Center - your logo, for example) */}
               <div className="column logo-column">
-                <img className="main-logo" src="./images/piedra_logo-2.svg" alt="Piedra Construcciones" />
+                <a href="#">
+                    <img className="main-logo" src="./images/piedra_logo-2.svg" alt="Piedra Construcciones" />
+                </a>
               </div>
 
               {/* 5) Fifth column */}
-              <div className="column"><a>REMODELACIONES</a> </div>
+              <div className="column"><a href='#'>REMODELACIONES</a> </div>
 
               {/* 6) Sixth column */}
-              <div className="column"><a>BLOG</a> </div>
+              <div className="column"><a href='#'>BLOG</a> </div>
 
               {/* 7) Seventh column: “Contactar” button */}
               <div className="column">
