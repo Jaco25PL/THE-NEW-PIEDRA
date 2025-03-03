41d97bf9b54f5661b31e5c13f3e223a571c14b0d
PiedraL <joaquinlangone12@gmail.com>
2025-03-03 12:24:08 -0300

    Call to action section, in 'remodelar' page done

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -35,9 +35,9 @@ export function CTA() {
       
       <div className={styles.testimonial}>
         <div className={styles.faces}>
-          <div className={styles.face}></div>
-          <div className={styles.face}></div>
-          <div className={styles.face}></div>
+          <div className={styles.face}><img src="./images/face-1.png" alt="" /></div>
+          <div className={styles.face}><img src="./images/face-2.png" alt="" /></div>
+          <div className={styles.face}><img src="./images/face-3.jpeg" alt="" /></div>
         </div>
         <div className={styles.ratings}>
           <p className={styles.ratingText}>150+ hogares renovados</p>
